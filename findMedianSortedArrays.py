1588ebe89f5bc08e9cc7d9695cf14189dc054204
Chris Hsu <chris.hsu@satisfly.com>
2015-03-16 14:50:35 +0800

    pass the submission

--- findMedianSortedArrays.py
+++ findMedianSortedArrays.py
@@ -1,6 +1,25 @@
 class Solution:
     # @return a float
     def findMedianSortedArrays(self, A, B):
-        medianPos    = len(A + B) / 2
-        medianPrePos = medianPos - 1 if len(A + B) % 2 == 0 else None
+        medianPos   = len(A + B) / 2
+        idxA = idxB = 0
+        mergedList = [] 
+        while len(mergedList) < medianPos + 1:
+              if idxA == len(A):
+                mergedList.append(B[idxB])
+                idxB += 1
+              elif idxB == len(B):
+                mergedList.append(A[idxA])
+                idxA += 1
+              else:
+                if A[idxA] >= B[idxB]:  
+                  mergedList.append(B[idxB])
+                  idxB += 1
+                else:
+                  mergedList.append(A[idxA])
+                  idxA += 1     
+        return float(mergedList[-1]) if len(A + B) % 2 == 1 else float(sum(mergedList[-2:])) / 2        
         
+s = Solution()
+s.findMedianSortedArrays(A, B)
+s.findMedianSortedArrays(C, D)

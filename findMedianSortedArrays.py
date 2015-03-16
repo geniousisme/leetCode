1b6b75a45add466a7de453a69eb3b3cbf2d34240
Chris Hsu <chris.hsu@satisfly.com>
2015-03-16 09:54:57 +0800

    add lines for findmedian

--- findMedianSortedArrays.py
+++ findMedianSortedArrays.py
@@ -1,4 +1,6 @@
 class Solution:
     # @return a float
     def findMedianSortedArrays(self, A, B):
+        medianPos    = len(A + B) / 2
+        medianPrePos = medianPos - 1 if len(A + B) % 2 == 0 else None
         

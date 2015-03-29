b332bfe7689d70c3c24daa5f7d26f5077cdb18f9
Chris Hsu <chris.hsu@satisfly.com>
2015-03-29 11:17:03 +0800

    finish longest common prefix

---
+++ longestCommonPrefix.py
@@ -0,0 +1,32 @@
+class Solution:
+    # @return a string
+    def longestCommonPrefix(self, strs):
+        common_prefix = ""
+        idx = 0
+        if len(strs) == 0: return common_prefix
+        while True:
+            current_char = ""
+            for string in strs:
+                if idx < len(string): # check if out of rang already
+                    if current_char == "": # no current string yet
+                        current_char = string[idx] # assign
+                    else: # hv current char to compare, we need to check
+                        if current_char == string[idx]:
+                            continue
+                        else: # no common anymore, return
+                            return common_prefix
+                else: # out of range for string, need to return
+                    return common_prefix
+            common_prefix += current_char #all strings hv char, add to prefix
+            idx += 1
+
+def main():
+    s = Solution()
+    strs = []
+    print s.longestCommonPrefix(strs)
+
+if __name__ == "__main__":
+    import time
+    start = time.clock()
+    main()
+    print "%s sec" % (time.clock() - start)

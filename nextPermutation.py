b3afc29094c34bde523e7dd50c3e816ef643e711
geniousisme <chia-hao.hsu@aiesec.net>
2015-04-26 17:28:25 +0800

    finish searchRange

--- nextPermutation.py
+++ nextPermutation.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 class Solution:
     # @param num, a list of integer
     # @return nothing (void), do not return anything, modify num in-place instead.
+    # 下面这种算法据说是STL中的经典算法。在当前序列中，从尾端往前寻找两个相邻升序元素，升序元素对中的前一个标记为partition。
+    # 然后再从尾端寻找另一个大于partition的元素，并与partition指向的元素交换，然后将partition后的元素（不包括partition指向的元素）
+    # 逆序排列。比如14532，那么升序对为45，partition指向4，由于partition之后除了5没有比4大的数，所以45交换为54，即15432，
+    # 然后将partition之后的元素逆序排列，即432排列为234，则最后输出的next permutation为15234。确实很巧妙。
     
     def nextPermutation(self, num):
         length = len(num); end = length - 1; partition = -1

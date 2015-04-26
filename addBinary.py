e6bc3a2cacd207827e8a7e68a8bd6ef51a262a6c
geniousisme <chia-hao.hsu@aiesec.net>
2015-04-26 10:35:03 +0800

    finish addBinary

--- addBinary.py
+++ addBinary.py
@@ -5,21 +5,23 @@ class Solution:
     def addBinary(self, a, b):
         result_binary = str(int(a) + int(b))
         result_binary = [int(b) for b in result_binary]
+        result_binary.insert(0, 0)
         length        = len(result_binary)
         carry         = 0
-        result_binary.insert(0, 0)
         for i in xrange(length - 1, -1, -1):
             result_binary[i] += carry
-            if result_binary[i] > 2:
+            if result_binary[i] > 1: # only can be 2 or 3
                result_binary[i] -= 2
                carry = 1
             else:
                carry = 0
+        if result_binary[0] == 0:
+           result_binary = result_binary[1:]
+        return ''.join([str(rb) for rb in result_binary])
+                
 
-1111
-1111
-
-02222
-11110
-
-        
+if __name__ == '__main__':
+   s = Solution()
+   a = '111'
+   b = '1'
+   print s.addBinary(a, b)

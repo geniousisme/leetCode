ca040ab7c7fe94a860f8970a159b18b59070dd20
StorySense <chris.hsu@storysensecomputing.com>
2015-04-29 13:50:59 +0800

    prepare to merge multiply

--- multiply.py
+++ multiply.py
@@ -2,5 +2,10 @@ class Solution:
     # @param {string} num1
     # @param {string} num2
     # @return {string}
-    def multiply(self, num1, num2):
+    def builtInMultiply(self, num1, num2):
         return str(int(num1) * int(num2))
+
+    def multiply(self, num1, num2):
+        if num2 > num1:
+           return self.multiply(num2, num1)
+        

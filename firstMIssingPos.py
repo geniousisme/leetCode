6a01975f7d45e9ac1fdd8cd91cf3f5996ba75d97
geniousisme <chia-hao.hsu@aiesec.net>
2015-05-03 14:36:58 +0800

    find missing postive is just too tricicky, cant understand now

--- firstMIssingPos.py
+++ firstMIssingPos.py
@@ -1,32 +1,41 @@
-class Solution:
-    # @param {integer[]} nums
-    # @return {integer}
-    def firstMissingPositive(self, nums):
-        length = len(nums); i = 0
-        while i < length:
-            if 1 <= nums[i] <= length and nums[nums[i] - 1] != nums[i]:
-               nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
-            else:
-               i += 1
-        for i in xrange(length):
-            if nums[i] != i + 1:
-               return i + 1
-        return length + 1
 # class Solution:
-# # @param A, a list of integers
-# # @return an integer
-# # @a very subtle solution
-# def firstMissingPositive(self, A):
-#     n=len(A)
-#     for i in range(n):
-#         if A[i]<=0: A[i]=n+2
-#     for i in range(n):
-#         if abs(A[i])<=n:
-#             curr=abs(A[i])-1
-#             A[curr]=-abs(A[curr])
-#     for i in range(n):
-#         if A[i]>0: return i+1
-#     return n+1
+#     # @param {integer[]} nums
+#     # @return {integer}
+#     def firstMissingPositive(self, nums):
+#         length = len(nums); i = 0
+#         while i < length:
+#             if 1 <= nums[i] <= length and nums[nums[i] - 1] != nums[i]:
+#                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+#             else:
+#                i += 1
+#             print nums
+#         print nums
+#         for i in xrange(length):
+#             if nums[i] != i + 1:
+#                return i + 1
+#         return length + 1
+
+class Solution:
+# @param A, a list of integers
+# @return an integer
+# @a very subtle solution
+      def firstMissingPositive(self, A):
+          n=len(A)
+          for i in xrange(n):
+              if A[i]<=0: A[i]=n+2
+          # print A
+          # print '-------------'
+          for i in xrange(n):
+              if abs(A[i])<=n:
+                  # print 'A[i]:', A[i]
+                  curr = abs(A[i])-1
+                  A[curr] = -abs(A[curr])
+                  # print 'A:', A
+          # print A                  
+          # print '============='
+          for i in xrange(n):
+              if A[i]>0: return i+1
+          return n+1
     
 if __name__ == '__main__':
    s = Solution()

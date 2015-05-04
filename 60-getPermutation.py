3d51dad1ad45727717abe1148cb634016f724e44
geniousisme <chia-hao.hsu@aiesec.net>
2015-05-05 00:22:29 +0800

    finish permutation sequence

--- 60-getPermutation.py
+++ 60-getPermutation.py
@@ -6,25 +6,26 @@ class Solution:
     # @return {string}
     def __init__(self):
         self.num_str_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-        self.res          = []
+
     def getPermutation(self, n, k):
-        nums = self.num_str_list[:n]
+        result_perm  = []
+        nums         = self.num_str_list[:n]
+        while nums:
+              start, index =  self.factorial_range(n, k)
+              result_perm.append(nums[start]) # optimized
+              nums = nums[:start] + nums[start + 1:] # optimized
+              k =  index
+              n -= 1
+        return ''.join(result_perm)
 
     def factorial_range(self, n, k):
         f = factorial(n - 1)
-        start = (k - 1) / f + 1
+        start = (k - 1) / f
         index = k % f 
         if not index: index = f
         return start, index
 
-
-
-
-
-        
-        
-    # def DFS()
-
 if __name__ == '__main__':
    s = Solution()
-   print s.factorial_range(5, )
+   # print s.factorial_range(2, 1)
+   print s.getPermutation(4, 5)

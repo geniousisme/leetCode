e29ada0e98d57a2d30906dcd805a709d344e4c68
geniousisme <chia-hao.hsu@aiesec.net>
2015-05-09 21:33:45 +0800

    add one more algorithm for this case, only use pointer version

--- 81-searchInRotatedSortedArray.py
+++ 81-searchInRotatedSortedArray.py
@@ -2,7 +2,7 @@ class Solution:
     # @param {integer[]} nums
     # @param {integer} target
     # @return {integer}
-    def search(self, nums, target):
+    def mySearch(self, nums, target):
         length         = len(nums)
         pivot          = self.get_pivot(nums)
         if pivot < 0: # there is no pivot
@@ -11,6 +11,8 @@ class Solution:
           post_res  = self.binarySearch(nums[pivot + 1:], 0, length - pivot - 1, target) # search in previous list 
           prev_res  = self.binarySearch(nums[:pivot + 1], 0, pivot + 1, target) # search in post list 
           return post_res or prev_res
+          # print nums[pivot + 1:] + nums[:pivot + 1] 
+          # return self.binarySearch(nums[pivot + 1:] + nums[:pivot + 1] , 0, length, target)
 
     def get_pivot(self, nums):
         for i in xrange(1, len(nums)):
@@ -32,9 +34,30 @@ class Solution:
         else:
              return True
 
+    def search(self, nums, target):
+        length = len(nums); left = 0; right = length - 1
+        while left <= right:
+              mid = (left + right) / 2
+              if nums[mid] == target: 
+                 return True
+              if nums[mid] == nums[left] == nums[right]:
+                   left  += 1
+                   right -= 1
+              elif nums[mid] >= nums[left]:
+                   if nums[mid] > target >= nums[left]: right = mid - 1
+                   else: left = mid + 1
+              else: # nums[mid] <= nums[left]:
+                   if nums[mid] <= target < nums[left]: left  = mid + 1
+                   else: right = mid - 1
+        return False
+
+
+
 if __name__ == '__main__':
    s = Solution()
-   print s.search([4, 5, 5, 6, 7, 0, 0, 1, 1, 2, 3, 3, 4, 4], 10)
-   print s.search([3, 1], 3)
-   
+   # nums1 = [4, 5, 5, 6, 7, 0, 0, 1, 1, 2, 3, 3, 4, 4]
+   # p = s.get_pivot(nums1)
+   # print nums1[:p]
+   print s.search([4, 5, 5, 6, 7, 0, 0, 1, 1, 2, 3, 3, 4, 4], 7)
+   print s.search([3, 1], 3)   
 

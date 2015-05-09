82f431940e48a1ee976b8da5032828fce3d56ff3
geniousisme <chia-hao.hsu@aiesec.net>
2015-05-09 18:08:50 +0800

    finish search in rotated array 2

--- 81-searchInRotatedSortedArray.py
+++ 81-searchInRotatedSortedArray.py
@@ -8,14 +8,9 @@ class Solution:
         if pivot < 0: # there is no pivot
           return self.binarySearch(nums, 0, length, target)
         else:      
-          post_idx  = self.binarySearch(nums[pivot + 1:], 0, length - pivot - 1, target) # search in previous list 
-          prev_idx  = self.binarySearch(nums[:pivot + 1], 0, pivot + 1, target) # search in post list 
-          if prev_idx > -1:
-             return prev_idx
-          elif post_idx > -1:
-             return post_idx + pivot + 1
-          else:
-             return -1
+          post_res  = self.binarySearch(nums[pivot + 1:], 0, length - pivot - 1, target) # search in previous list 
+          prev_res  = self.binarySearch(nums[:pivot + 1], 0, pivot + 1, target) # search in post list 
+          return post_res or prev_res
 
     def get_pivot(self, nums):
         for i in xrange(1, len(nums)):
@@ -26,20 +21,20 @@ class Solution:
     def binarySearch(self, nums, start, end, target):
         if len(nums[start:end]) == 1:
            if nums[0] == target:
-              return start
+              return True
            else:
-              return -1
+              return False
         mid = (start + end) / 2
         if target > nums[mid]:
            return self.binarySearch(nums, mid, end, target)
         elif target < nums[mid]:
              return self.binarySearch(nums, start, mid, target)
         else:
-             return mid
+             return True
 
 if __name__ == '__main__':
    s = Solution()
-   print s.search([4, 5, 6, 0, 1, 2, 3], 1)
-   print s.search([3, 1], 0)
+   print s.search([4, 5, 5, 6, 7, 0, 0, 1, 1, 2, 3, 3, 4, 4], 10)
+   print s.search([3, 1], 3)
    
 

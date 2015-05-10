d4fd86b95f6633106c00bb9ae4ffa58b7bfe07a7
geniousisme <chia-hao.hsu@aiesec.net>
2015-05-10 20:11:58 +0800

    finish deleteduplicates in sorted list

--- 82-deleteDuplicates.py
+++ 82-deleteDuplicates.py
@@ -8,13 +8,43 @@ class Solution:
     # @param {ListNode} head
     # @return {ListNode}
     def deleteDuplicates(self, head):
-        start = ListNode(0); start.next = head; tmp = head
-        prev  = None; prev_prev = None
+        if head is None or head.next is None: return head
+        start = ListNode(-99); start.next = head 
+        prev = start
+        post = start.next
+        while prev.next:
+              while post.next and prev.next.val == post.next.val:
+                    post = post.next
+              if post == prev.next:
+                 prev = prev.next
+                 post = prev.next
+              else:
+                 prev.next = post.next
+        return start.next
+              
+    def print_llst(self, head):
+        llst = ""
         while head:
-              if prev != start.next.val
-                 prev = start.next.val
-                 start.next = start.next.next
-              else: # prev == start.next.val
+              llst += str(head.val)
+              if head.next:
+                 llst += '->'
+              head = head.next
+        print llst
+
+if __name__ == '__main__':
+   s = Solution()
+   test = ListNode(1)
+   test.next = ListNode(1)
+   test.next.next = ListNode(1)
+   test.next.next.next = ListNode(2)
+   test.next.next.next.next = ListNode(2)
+   test.next.next.next.next.next = ListNode(4)
+   test.next.next.next.next.next.next = ListNode(4)
+   # s.deleteDuplicates(test)
+   s.print_llst(s.deleteDuplicates(test))
+   
+
+
 
 
 

9a1b90bef27500bd67bc485f0160ad110a746eab
geniousisme <chia-hao.hsu@aiesec.net>
2015-05-17 19:08:59 +0800

    finish unique binary tree

--- 119-pascalTriangle2.py
+++ 119-pascalTriangle2.py
@@ -10,6 +10,7 @@ class Solution:
         for k in xrange(rowIndex + 1):
             res.append(f(n) / f(k) / f(n - k))
         return res
+        
 if __name__ == '__main__':
    s = Solution()
    for i in xrange(10):

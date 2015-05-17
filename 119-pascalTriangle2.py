08743e2efd10405f7b3c14f30beda280fd4f851d
geniousisme <chia-hao.hsu@aiesec.net>
2015-05-17 14:38:28 +0800

    finish pascal trianle - get row

--- 119-pascalTriangle2.py
+++ 119-pascalTriangle2.py
@@ -1,5 +1,20 @@
+from math import factorial
+
 class Solution:
     # @param {integer} rowIndex
     # @return {integer[]}
     def getRow(self, rowIndex):
+        res = []
+        f = factorial
+        n = rowIndex
+        for k in xrange(rowIndex + 1):
+            res.append(f(n) / f(k) / f(n - k))
+        return res
+if __name__ == '__main__':
+   s = Solution()
+   for i in xrange(10):
+       print s.getRow(i)
+
+
+
         

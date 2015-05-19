62851f75ebde83b977494ce6e0f6699c587b9872
StorySense <chris.hsu@storysensecomputing.com>
2015-05-19 10:44:47 +0800

    finish contruct binary tree for sorted array

--- 108-sortedArrayToBST.py
+++ 108-sortedArrayToBST.py
@@ -9,4 +9,17 @@ class Solution:
     # @param {integer[]} nums
     # @return {TreeNode}
     def sortedArrayToBST(self, nums):
-        
+        return self.buildTree(nums, 0, len(nums) - 1)
+
+    def buildTree(self, nums, start, end):
+        if start > end: return None
+        mid = (start + end) / 2
+        root = TreeNode(nums[mid]) 
+        root.right = self.buildTree(nums, mid + 1, end)
+        root.left  = self.buildTree(nums, start, mid - 1)
+        return root
+
+if __name__ == '__main__':
+   s = Solution()
+   print s.sortedArrayToBST([1, 2, 3, 4, 5, 6, 7, 8])
+

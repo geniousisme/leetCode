4270e656651f41d224225a622a97501fd6d5fff9
StorySense <chris.hsu@storysensecomputing.com>
2015-06-02 14:45:36 +0800

    finish 163 - missing range

---
+++ 207-courseSchedule.py
@@ -0,0 +1,7 @@
+class Solution:
+    # @param {integer} numCourses
+    # @param {integer[][]} prerequisites
+    # @return {boolean}
+    def CourseNode(self, course_num):
+        self.val = course_num
+        sel.next =  

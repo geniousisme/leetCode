231bfd8b20ff1f7aa5855fb4d4f5564620aba393
StorySense <chris.hsu@storysensecomputing.com>
2015-06-09 12:36:59 +0800

    start reverse linked list

---
+++ 206-reversedLinkedList.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param {ListNode} head
+    # @return {ListNode}
+    def reverseList(self, head):
+        start = ListNode(-1); last = start.next
+        while head:
+
+            
+              

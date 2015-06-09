843b28eba3c80eec30b51733505b9600111b819f
StorySense <chris.hsu@storysensecomputing.com>
2015-06-09 14:19:31 +0800

    finish reverse linked list

--- 206-reversedLinkedList.py
+++ 206-reversedLinkedList.py
@@ -8,8 +8,35 @@ class Solution:
     # @param {ListNode} head
     # @return {ListNode}
     def reverseList(self, head):
-        start = ListNode(-1); last = start.next
+        if head is None or head.next is None:
+           return head
+        new_node = ListNode(-1); last = None
         while head:
+              new_node = ListNode(head.val) 
+              new_node.next = last
+              last = new_node
+              head = head.next
+        return new_node
 
-            
+    def print_llst(self, head):
+        llst = ""
+        while head:
+              llst += str(head.val)
+              if head.next:
+                 llst += '->'
+              head = head.next
+        print llst
+
+if __name__ == '__main__':
+   s = Solution()
+   test = ListNode(1)
+   test.next = ListNode(2)
+   test.next.next = ListNode(3)
+   test.next.next.next = ListNode(4)
+   test.next.next.next.next = ListNode(5)
+
+   s.print_llst(s.reverseList(test))
+
+
+
               

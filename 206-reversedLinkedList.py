e2cd4954d05c57e34a6d9bd9c7edc7f589f5fc13
StorySense <chris.hsu@storysensecomputing.com>
2015-06-10 12:43:51 +0800

    finish reverse linked list

--- 206-reversedLinkedList.py
+++ 206-reversedLinkedList.py
@@ -1,22 +1,32 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
     # @param {ListNode} head
     # @return {ListNode}
+    # def reverseList(self, head):
+    #     if head is None or head.next is None:
+    #        return head
+    #     new_node = ListNode(-1); last = None
+    #     while head:
+    #           new_node = ListNode(head.val) 
+    #           new_node.next = last
+    #           last = new_node
+    #           head = head.next
+    #     return new_node
+
     def reverseList(self, head):
-        if head is None or head.next is None:
-           return head
-        new_node = ListNode(-1); last = None
-        while head:
-              new_node = ListNode(head.val) 
-              new_node.next = last
-              last = new_node
-              head = head.next
-        return new_node
+        p = head
+        start = None
+        while p:
+            next = p.next
+            p.next = start
+            start = p
+            p = next
+        return start
 
     def print_llst(self, head):
         llst = ""
@@ -35,7 +45,7 @@ if __name__ == '__main__':
    test.next.next.next = ListNode(4)
    test.next.next.next.next = ListNode(5)
 
-   s.print_llst(s.reverseList(test))
+   s.print_llst(s.reverse(test))
 
 
 

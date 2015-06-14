dc4817caf5570a52e4282cf22d3c6f3a0227b1a8
geniousisme <chia-hao.hsu@aiesec.net>
2015-06-14 15:47:35 +0800

    finish rectangle area

--- 223-rectangleArea.py
+++ 223-rectangleArea.py
@@ -11,8 +11,19 @@ class Solution:
     def computeArea(self, A, B, C, D, E, F, G, H):
         area1   = (C - A) * (D - B)
         area2   = (G - E) * (H - F)
-        if area1 == 0 or area2 == 0 or A > G or B > H or E > C or B > H:
+        if E >= C or F >= D or A >= G or B >= H:
            overlap = 0
         else:
-           overlap = (C - E) * (H - B)
+           overlap = self.overlapLen([A, E, C, G]) * self.overlapLen([D, H, B, F])
         return area1 + area2 - overlap
+    
+    def overlapLen(self, nums):
+        nums.sort()
+        return nums[2] - nums[1]
+
+
+if __name__ == '__main__':
+   s = Solution()
+   print s.computeArea(0,0,0,0, -2, -2, 2, 2)
+   print s.computeArea(-2, -2, 2, 2, 3, 3, 4, 4)
+   print s.computeArea(-2, -2, 2, 2, -4, -4, -3, -3)

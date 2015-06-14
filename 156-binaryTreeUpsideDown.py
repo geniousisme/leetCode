909082cb6be511cdb2ba498b1701c6b1a4e0683b
geniousisme <chia-hao.hsu@aiesec.net>
2015-06-15 00:49:59 +0800

    try to rewrite binary tree upside down

--- 156-binaryTreeUpsideDown.py
+++ 156-binaryTreeUpsideDown.py
@@ -4,45 +4,22 @@ class TreeNode:
         self.val = x
         self.left = None
         self.right = None
-
-class Solution:
-    # @param {TreeNode} root
-    # @return {TreeNode}
-    def upsideDownBinaryTree(self, root):
-        tree_order = []
-        start = root
-        if root:
-           tree_order.append(root.val)
-           while root:
-                 if root.right:
-                    tree_order.append(root.right.val)
-                 if root.left:
-                    tree_order.append(root.left.val)
-                 root = root.left
-           print tree_order
-           new = TreeNode(tree_order.pop()); start = new
-           while tree_order:
-                 new.left = TreeNode(tree_order.pop())
-                 new.right = TreeNode(tree_order.pop())
-                 new = new.right
-
-        return start
     
 # Chris:TODO::write following again, so sleepy.
 class Solution:
     # @param root, a tree node
     # @return root of the upside down tree
     def upsideDownBinaryTree(self, root):
-        p, parent, parent_right = root, None, None
-        
-        while p:
-            left = p.left
-            p.left = parent_right
-            parent_right = p.right
-            p.right = parent
-            parent = p
-            p = left
-            
+        parent = None
+        parent_left = None
+        while root:
+              left = root.left
+              right = root.right
+              root.left = parent
+              parent = root
+              root.right = parent_left
+              parent_left = right
+              root = left
         return parent
 
 if __name__ == '__main__':
@@ -50,9 +27,9 @@ if __name__ == '__main__':
    
    test = TreeNode(1)
    test.left = TreeNode(2)
-   # test.right = TreeNode(3)
-   # test.left.left = TreeNode(4)
-   # test.left.right = TreeNode(5)
+   test.right = TreeNode(3)
+   test.left.left = TreeNode(4)
+   test.left.right = TreeNode(5)
 
    print s.upsideDownBinaryTree(test)
 

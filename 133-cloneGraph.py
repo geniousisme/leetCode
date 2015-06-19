892e07fb6ebc28e21477039d8ef0f6e2ae73c1f8
geniousisme <chia-hao.hsu@aiesec.net>
2015-06-19 23:32:47 +0800

    finish clone graph bfs

--- 133-cloneGraph.py
+++ 133-cloneGraph.py
@@ -4,13 +4,16 @@
 #         self.label = x
 #         self.neighbors = []
 
+
+# Chris:TODO::please write this program again by yourself.
+
 class Solution:
     # @param node, a undirected graph node
     # @return a undirected graph node
     def cloneGraph(self, node):
         if node is None:
            return node
-        return self.dfs(node, {})
+        return self.bfs(node, {})
 
     def dfs(self, node, table):
         if table.get(node) is not None:
@@ -20,5 +23,21 @@ class Solution:
         for neighbor in node.neighbors:
             next_node.neighbors.append(self.dfs(neighbor, table))
         return next_node
+
+    def bfs(self, node, table):
+        queue = [node]
+        newhead = UndirectedGraphNode(node.label)
+        table[node] = newhead
+        while queue:
+              curr_node = queue.pop()
+              for neighbor in curr_node.neighbors:
+                  if table.get(neighbor) is None:
+                     new_node = UndirectedGraphNode(neighbor.label)
+                     table[curr_node].neighbors.append(new_node)
+                     table[neighbor] = new_node
+                     queue.append(neighbor)
+                  else:
+                     table[curr_node].neighbors.append(table[neighbor])
+        return newhead
 
         

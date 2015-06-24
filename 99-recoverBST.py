4f662500d68261ec01665e71794e7222501e7a14
geniousisme <chia-hao.hsu@aiesec.net>
2015-06-25 00:06:40 +0800

    add the O(1) soluntion for recover tree

--- 99-recoverBST.py
+++ 99-recoverBST.py
@@ -8,6 +8,24 @@ class TreeNode:
 class Solution:
     # @param {TreeNode} root
     # @return {void} Do not return anything, modify root in-place instead.
+
+    # Chris:TODO::study this O(1) space complexity solution.
+    def FindTwoNodes(self, root):
+            if root:
+                self.FindTwoNodes(root.left)
+                if self.prev and self.prev.val > root.val:
+                    self.n2 = root
+                    if self.n1 == None: self.n1 = self.prev
+                self.prev = root
+                self.FindTwoNodes(root.right)
+    def recoverTree(self, root):
+        self.n1 = self.n2 = None
+        self.prev = None
+        self.FindTwoNodes(root)
+        self.n1.val, self.n2.val = self.n2.val, self.n1.val
+        return root
+
+
     def inorderTraversal(self, root, val_list, node_list):
         if root:
            self.inorderTraversal(root.left, val_list, node_list)

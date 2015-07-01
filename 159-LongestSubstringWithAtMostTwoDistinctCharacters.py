d3055e2ca64eae9d9f74399b835128d3c4ead39f
chrishsu <chia-hao.hsu@aiesec.net>
2015-07-01 23:34:43 +0800

    finish largest substring with distinct chars

--- 159-LongestSubstringWithAtMostTwoDistinctCharacters.py
+++ 159-LongestSubstringWithAtMostTwoDistinctCharacters.py
@@ -6,7 +6,6 @@ class Solution:
         two_word_dict     = {}
         distinct_count    = 0
         max_len = tmp_len = 0
-        diff_idx          = 0
         if s:
            for i in xrange(len(s)):
                if two_word_dict.get(s[i], False):
@@ -18,24 +17,21 @@ class Solution:
                      two_word_dict = {}
                      two_word_dict[s[i]] = True
                      two_word_dict[s[i - 1]] = True
-                     if s[diff_idx] != s[i - 1]:
-                        diff_idx = i - 1
-                     tmp_len = 1 + i - diff_idx
-                     diff_idx = i
+                     j = i - 1
+                     while j > 0 and s[j] == s[j - 1]:
+                           j -= 1
+                     tmp_len = 1 + i - j
                   else:                     
-                     diff_idx = i
-                     # print 'diff_idx', diff_idx
                      two_word_dict[s[i]] = True
                      tmp_len += 1
-               # print s[i], 'tmp_len', tmp_len
                max_len = max(max_len, tmp_len)
         return max_len
 
 if __name__ == '__main__':
    s = Solution()
    print s.lengthOfLongestSubstringTwoDistinct('eceba')
-   # print s.lengthOfLongestSubstringTwoDistinct('eecebaaaaa')
-   # print s.lengthOfLongestSubstringTwoDistinct('ee')
+   print s.lengthOfLongestSubstringTwoDistinct('eecebaaaaa')
+   print s.lengthOfLongestSubstringTwoDistinct('ee')
    print s.lengthOfLongestSubstringTwoDistinct("abaccc")
    print s.lengthOfLongestSubstringTwoDistinct('ccaabbb')
 

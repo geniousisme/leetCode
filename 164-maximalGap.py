d59087df2a606b1d8df15580d9a3dc747061ab2b
chrishsu <chia-hao.hsu@aiesec.net>
2015-07-05 23:10:20 +0800

    add more solutions

--- 164-maximalGap.py
+++ 164-maximalGap.py
@@ -30,47 +30,12 @@ class Solution:
               i = next_i
         return maxGap
 
-# class Solution:
-#     # @param num, a list of integer
-#     # @return an integer
-#     def maximumGap(self, num):
-#         N = len(num)
-#         if N < 2:
-#             return 0
-#         A = min(num)
-#         B = max(num)
-#         bucketRange = max(1, int((B - A - 1) / (N - 1)) + 1) #ceil( (B - A) / (N - 1) )
-#         bucketLen = (B - A) / bucketRange + 1
-#         buckets = [None] * bucketLen
-#         for K in num:
-#             loc = (K - A) / bucketRange
-#             bucket = buckets[loc]
-#             if bucket is None:
-#                 bucket = {'min' : K, 'max' : K}
-#                 buckets[loc] = bucket
-#             else:
-#                 bucket['min'] = min(bucket['min'], K)
-#                 bucket['max'] = max(bucket['max'], K)
-#         maxGap = 0
-#         print buckets
-#         for x in range(bucketLen):
-#             if buckets[x] is None:
-#                 continue
-#             y = x + 1
-#             while y < bucketLen and buckets[y] is None:
-#                 y += 1
-#             if y < bucketLen:
-#                 maxGap = max(maxGap, buckets[y]['min'] - buckets[x]['max'])
-#             x = y
-#         return maxGap
-
-    # def maximumGap(self, nums):
-    #     nums.sort()
-    #     maxGap = 0
-    #     print nums
-    #     for i in xrange(1, len(nums)):
-    #         maxGap = max(maxGap, nums[i] - nums[i - 1])
-    #     return maxGap
+    def sortedMaximumGap(self, nums):
+        nums.sort()
+        maxGap = 0
+        for i in xrange(1, len(nums)):
+            maxGap = max(maxGap, nums[i] - nums[i - 1])
+        return maxGap
 
 if __name__ == '__main__':
    s = Solution()

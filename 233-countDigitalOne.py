afdd48f999734c010d7d495a3df1087fe7cb1486
chrishsu <chia-hao.hsu@aiesec.net>
2015-07-08 18:24:08 +0800

    add more comments for he question

--- 233-countDigitalOne.py
+++ 233-countDigitalOne.py
@@ -3,11 +3,16 @@ class Solution:
     # @return {integer}
     # take 
     def countDigitOne(self, n):
+        # Chris:TODO::take this as reference 
+        # http://www.cnblogs.com/grandyang/p/4629032.html
         res = 0; a = b = 1
         while n > 0:
-              print 'a', a
-              print 'b', b
+              # print 'a', a
+              # print 'b', b
               res += (n + 8) / 10 * a + (n % 10 == 1) * b
+              # print 'res', res
+              # a in charge of ten order, should add 10 or not, b in charge of the 
+              # from unit to ten-th unit, (n % 10 == 1) deal with the special case, ex. 11, 111, 1111
               b   += n % 10 * a
               a   *= 10
               n /= 10
@@ -16,8 +21,12 @@ class Solution:
 if __name__ == '__main__':
    s = Solution()
    print s.countDigitOne(11)
+   print s.countDigitOne(19)
+
    print s.countDigitOne(56)
    print s.countDigitOne(101)
+   print s.countDigitOne(111)
+
    print s.countDigitOne(1111)
 
 

0d72c5bf81e4094a8fdbb0e5b4b2e1cac85f71c3
chrishsu <chia-hao.hsu@aiesec.net>
2015-07-14 21:11:56 +0800

    finish basic calculator II

--- 227-basicCaculatorII.py
+++ 227-basicCaculatorII.py
@@ -5,57 +5,60 @@ class Solution:
     # http://www.wengweitao.com/leetcode-basic-calculator.html
     def calculate(self, s):
         length = len(s)
-        if length == 0: return 0
-        ans = 0
+        if length == 0: 
+           return 0
+        ans = last_num = op = i = 0
         num = ''
-        stack = []
         sign = 1
-        op   = 0
-
-        for i in xrange(length):
-            if '9' >= s[i] >= '0':
-               num += s[i]
+        # op = 0: +, -
+        # op = 1: *
+        # op = 2: /
+        while i < length:
+            if s[i] == ' ':
+               i += 1
+               continue
             
-            if   op == 0:
-                 ans = 
-            elif op == 1:
-                 ans = last_num * i
-            elif op == 2:
-                 ans = 
-            else: # op == 3:
-                 ans = 
+            if '9' >= s[i] >= '0':
+                num = ''
+                while i < length and '9' >= s[i] >= '0':
+                      num += s[i]
+                      i += 1
+                i -= 1
+                if op == 1:
+                   last_num *= int(num)
+                elif op == 2:
+                     last_num /= int(num)
+                else: # op == 0, +, -
+                     last_num = int(num)
 
-            elif s[i] == '+':
-                 ans += sign * int(num)
-                 sign = 1
-                 num  = ''
+            if s[i] == '+':
+               ans += sign * last_num
+               op   = 0 
+               sign = 1
 
             elif s[i] == '-':
-                 ans += sign * int(num)
+                 ans += sign * last_num
+                 op   = 0
                  sign = -1
-                 num  = ''
             
             elif s[i] == '*':
-                 stack.append(ans)
-                 stack.append(sign)
-                 sign = 1
-                 ans  = 0
-
+                 op   = 1
+                 
             elif s[i] == '/':
-                 ans += sign * int(num)
-                 sign = stack.pop()
-                 ans  = sign * ans + stack.pop()
-                 num  = 0
-
-        if num != 0:
-           ans += sign * int(num)
+                 op   = 2
+            
+            i += 1
+        
+        ans += sign * last_num
 
         return ans
             
 
 if __name__ == '__main__':
    s = Solution()
-   print s.calculate('234567890')
-   print s.calculate("1 + 1")
-   print s.calculate(" 2-1 + 2 ")
-   print s.calculate("(1+(4+5+2)-3)+(6+8)")
+   # print s.calculate("3+2*2")
+   # print s.calculate(" 3/2 ")
+   # print s.calculate(" 3+5 / 2 ")
+   print s.calculate("2*3+4")
+
+   

e3b3e7a1f7e5a4e5e4b31b946ec8f4248ee39d24
chrishsu <chia-hao.hsu@aiesec.net>
2015-07-15 16:13:00 +0800

    add comments and test cases to read4 i & II

--- 157-readNCharsByRead4.py
+++ 157-readNCharsByRead4.py
@@ -2,11 +2,23 @@
 # @param buf, a list of characters
 # @return an integer
 # def read4(buf):
+def read4(buf):
+    global file_content
+    i = 0
+    while i < len(file_content) and i < 4:
+        buf[i] = file_content[i]
+        i += 1
+    
+    if len(file_content) > 4:
+        file_content = file_content[4:]
+    else:
+        file_content = ""
+    return i
 
 class Solution:
     # @param buf, Destination buffer (a list of characters)
-    # @param n,   Maximum number of characters to read ａ(an integer)
-    # @return     The number of characters read (an integer)
+    
+    # @return The number of characters read (an integer)
     
     # Chris:TODO::WTF, still dont understand the read4 function
     def read(self, buf, n):
@@ -21,4 +33,10 @@ class Solution:
                  return idx
 
 
-        
+if __name__ == "__main__":
+    global file_content
+    buf = ['' for _ in xrange(100)]
+    file_content = "a"
+    print buf[:Solution().read(buf, 9)]    
+    file_content = "abcdefghijklmnop"
+    print buf[:Solution().read(buf, 9)]    
--- 158-readNCharsByRead4II.py
+++ 158-readNCharsByRead4II.py
@@ -2,10 +2,49 @@
 # @param buf, a list of characters
 # @return an integer
 # def read4(buf):
+def read4(buf):
+    global file_content
+    i = 0
+    while i < len(file_content) and i < 4:
+        buf[i] = file_content[i]
+        i += 1
+    
+    if len(file_content) > 4:
+        file_content = file_content[4:]
+    else:
+        file_content = ""
+    return i
 
 class Solution:
     # @param buf, Destination buffer (a list of characters)
     # @param n,   Maximum number of characters to read (an integer)
     # @return     The number of characters read (an integer)
+    def __init__(self):
+        self.buffer = ['', '', '', '']
+        self.offset = self.bufsize = 0
+    
     def read(self, buf, n):
+        read_bytes = 0
+        eof        = False
+        while not eof and read_bytes < n:
+              if self.bufsize == 0:
+                 self.bufsize = read4(self.buffer)
+                 eof          = self.bufsize < 4
+              bytes = min(n - read_bytes, self.bufsize)
+              for i in xrange(bytes):
+                  buf[read_bytes + i] = self.buffer[self.offset + i]
+              self.offset = (self.offset + bytes) % 4
+              self.bufsize -= bytes
+              read_bytes += bytes
+
+        return read_bytes
+
+if __name__ == "__main__":
+    global file_content
+    sol = Solution()
+    buf = ['' for _ in xrange(100)]
+    file_content = "ab"
+    print buf[:sol.read(buf, 1)]
+    print buf[:sol.read(buf, 2)]    
+
         

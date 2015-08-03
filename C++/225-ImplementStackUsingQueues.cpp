0d8c0b76a5ee8a93d1640e508a040d2efdb57204
chrishsu <chia-hao.hsu@aiesec.net>
2015-08-03 17:20:15 +0800

    finish implement stack using queue

--- C++/225-ImplementStackUsingQueues.cpp
+++ C++/225-ImplementStackUsingQueues.cpp
@@ -2,21 +2,38 @@ class Stack {
 public:
     // Push element x onto stack.
     void push(int x) {
-        
+         myQueue.push(x);
     }
-
     // Removes the element on top of the stack.
     void pop() {
-        
+         while (!myQueue.empty()) {
+                tmpV.push_back(myQueue.front());
+                myQueue.pop();
+         };
+         tmpV.pop_back();
+         for (int i = 0; i < tmpV.size(); i++) {
+              myQueue.push(tmpV[i]);
+         };
+         tmpV.clear();
     }
-
     // Get the top element.
     int top() {
-        
+        while (!myQueue.empty()) {
+                tmpV.push_back(myQueue.front());
+                myQueue.pop();
+         };
+         int top = tmpV.back();
+         for (int i = 0; i < tmpV.size(); i++) {
+              myQueue.push(tmpV[i]);
+         };
+         tmpV.clear();
+        return top;
     }
-
     // Return whether the stack is empty.
     bool empty() {
-        
+         return myQueue.empty();        
     }
+private:
+    queue<int> myQueue;
+    vector<int> tmpV;
 };
--- C++/232-ImplementQueueUsingStacks.cpp
+++ C++/232-ImplementQueueUsingStacks.cpp
@@ -4,41 +4,41 @@ class Queue {
 public:
     // Push element x to the back of queue.
     void push(int x) {
-         stk.push(x);
+         myStack.push(x);
     }
 
     // Removes the element from in front of queue.
     void pop(void) {
-         while (!stk.empty()) {
-                tmpV.push_back(stk.top());
-                stk.pop();
+         while (!myStack.empty()) {
+                tmpV.push_back(myStack.top());
+                myStack.pop();
          };
          tmpV.pop_back();
          for (int i = tmpV.size() - 1; i > -1; i--) {
-              stk.push(tmpV[i]);
+              myStack.push(tmpV[i]);
               tmpV.pop_back();
          };
     }
 
     // Get the front element.
     int peek(void) {
-        while (!stk.empty()) {
-               tmpV.push_back(stk.top());
-               stk.pop();
+        while (!myStack.empty()) {
+               tmpV.push_back(myStack.top());
+               myStack.pop();
         };
-        int top = tmpV.back();
+        int peek = tmpV.back();
         for (int i = tmpV.size() - 1; i > -1; i--) {
-             stk.push(tmpV[i]);
+             myStack.push(tmpV[i]);
         };
         tmpV.clear();
-        return top;
+        return peek;
     }
     // Return whether the queue is empty.
     bool empty(void) {
-         return stk.empty();  
+         return myStack.empty();  
     }
 private:
-    stack<int> stk;
+    stack<int> myStack;
     vector<int> tmpV;
 };
 

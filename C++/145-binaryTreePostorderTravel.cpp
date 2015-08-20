530f1cde1bd5d7c5ee8ee32f3671b7dc7f099275
chrishsu <chia-hao.hsu@aiesec.net>
2015-08-20 18:06:26 -0400

    finish postorder traversal

--- C++/145-binaryTreePostorderTravel.cpp
+++ C++/145-binaryTreePostorderTravel.cpp
@@ -1,3 +1,4 @@
+#include "include/common.h"
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -9,44 +10,53 @@
  */
 class Solution {
 public:
-    vector<int> postorderTraversalI(TreeNode* root) {
-                vector<int> res;
-                traverse(root, res);
-                return res;
-
-    }
-    void traverse(TreeNode* root, vector<int>& res) {
-         if (root) {
-             traverse(root->left, res);
-             traverse(root->right, res);
-             res.push_back(root->val);       
-         };
-         return;
-    }
     vector<int> postorderTraversal(TreeNode* root) {
                 vector<int> res;
                 iterTraverse(root, res);
                 return res;
+
+    }
+private:
+    void recTraverse(TreeNode* root, vector<int>& res) {
+         if (root) {
+             recTraverse(root->left, res);
+             recTraverse(root->right, res);
+             res.push_back(root->val);       
+         };
+         return;
     }
     void iterTraverse(TreeNode* root, vector<int>& res) {
          vector<TreeNode*> stack;
-         TreeNode* prev = nullptr, curr = nullptr;
-         if (root) {
-             stack.push_back(root);
-             while(!stack.empty()) {
-                   curr = stack[stack.size() - 1];
-                   if ((curr->right == nullptr && curr->left == nullptr) || (prev && (prev == curr->right || prev == curr->right))) {
-                        
-                        stack.pop();
-                   }
-                   else {
-
-                   };
-
-             };
+         TreeNode *prev = nullptr, *curr = nullptr;
+         if (root == nullptr) {
+             return;
          };
+         stack.push_back(root);
+         while (!stack.empty()) {
+                curr = stack[stack.size() - 1];
+                if ((curr->right == nullptr && curr->left == nullptr) || (prev && (prev == curr->right || prev == curr->left))) {
+                    res.push_back(curr->val);
+                    prev = curr;
+                    stack.pop_back();
+                }
+                else {
+                    if (curr->right) {
+                        stack.push_back(curr->right);
+                    };
+                    if (curr->left) {
+                        stack.push_back(curr->left);
+                    };
+                };
+         };
+         return;
     }
-
-    
-
 };
+
+int main(void) {
+    Solution s;
+    TreeNode root = TreeNode(1);
+    // root.left = &TreeNode(2);
+    // root.right = &TreeNode(3);
+    s.postorderTraversal(&root);
+    return 0;
+}

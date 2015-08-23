c61ba244f0200a3400816e50d17902d46066f723
chrishsu <chia-hao.hsu@aiesec.net>
2015-08-23 11:07:35 -0400

    finish sort color second solution

--- C++/75-sortColor.cpp
+++ C++/75-sortColor.cpp
@@ -2,7 +2,7 @@
 
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
+    void sortColorsI(vector<int>& nums) {
          int zeroPtr = 0, i = 0, twoPtr = nums.size() - 1;
          while (i <= twoPtr) {
                 if (nums[i] == 0) {
@@ -19,6 +19,19 @@ public:
                 };
          };
     }
+    void sortColors(vector<int>& nums) {
+         int count [] = {0, 0, 0};
+         for (int i = 0; i < nums.size(); i++) {
+              count[nums[i]]++;
+         };
+         for (int i = 0, j = 0; i < 3 && j < nums.size(); i++) {
+              while (count[i] > 0) {
+                     nums[j] = i;
+                     count[i]--;
+                     j++;
+              };
+         };
+    };
 private:
     int tmp;
     void swap(vector<int>& nums, int ptr1, int ptr2) {

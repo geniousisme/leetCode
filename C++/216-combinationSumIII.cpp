c5d136719bf635c59aac9df14fcfe2c4c8476140
chrishsu <chia-hao.hsu@aiesec.net>
2015-08-30 00:23:45 -0400

    finish the bug of merge sorted array

---
+++ C++/216-combinationSumIII.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        
+    }
+private:
+    void combSumHelper(vector<vector<int> >& res, vector<int>& comb, ) {
+
+    }
+};
--- C++/88-mergedSortedArray.cpp
+++ C++/88-mergedSortedArray.cpp
@@ -1,3 +1,5 @@
+#include "include/common.h"
+
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
@@ -18,10 +20,26 @@ public:
                     idx2++;
                 };
                 i++;
-         };         
-         if (idx2 < n) {
-             nums1[total_len + idx2 - n] = nums2[idx2];
-             idx2++;
+         };
+         // cout << "idx2: " << idx2 << endl;
+         // cout << nums2[idx2] << endl;
+         // cout << "n: " << n << endl;         
+         while (idx2 < n) {
+                // cout << "nums2[" << idx2 << "]: " << nums2[idx2] << endl;
+                nums1[total_len + idx2 - n] = nums2[idx2];
+                idx2++;
          };
     }
+};
+
+int main(void) {
+    Solution s;
+    Utils utils;
+    int arr1 [] = {1, 2, 3, 0, 0, 0};
+    vector<int> nums1(arr1, arr1 + sizeof(arr1) / sizeof(arr1[0]));
+    int arr2 [] = {2, 5, 6};
+    vector<int> nums2(arr2, arr2 + sizeof(arr2) / sizeof(arr2[0]));
+    s.merge(nums1, 3, nums2, 3);
+    utils.printIntVector(nums1);
+    return 0;
 };

6e12d67e4ab2d197d588b65c1ddb1f9c52a7e047
chrishsu <chia-hao.hsu@aiesec.net>
2015-09-23 12:15:05 -0400

    finish robberHouse problem II opt solution

--- C++/109-ConvertSortedListToBST.cpp
+++ C++/109-ConvertSortedListToBST.cpp
@@ -19,27 +19,10 @@
 class Solution {
 public:
     TreeNode* sortedListToBST(ListNode* head) {
-      int listLength = 0;
-      ListNode *dummy = head;
-      while (dummy) {
-        dummy = dummy->next;
-        listLength++;
-      };
-      return buildBST(head, 0, listLength - 1);
+      
     }
 private:
-    TreeNode *buildBST(ListNode *head, int start, int end) {
-        if (start > end) {
-          return nullptr;
-        };
-        int mid = (start + end) / 2;
-        TreeNode *leftChild = buildBST(head, start, mid - 1);
-        TreeNode *root = new TreeNode(head->val);
-        head = head->next;
-        TreeNode *rightChild = buildBST(head, mid + 1, end);
-        root->left = leftChild;
-        root->right = rightChild;
-        return root;
+    TreeNode* buildBST() {
 
     }
 };
--- C++/213-robberHouseII.cpp
+++ C++/213-robberHouseII.cpp
@@ -6,31 +6,59 @@ using namespace std;
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        // if you robber the first one, then you can't robber the last one
-        vector<int> nums1(nums.begin(), nums.end() - 1);
-        // if you robber the last one, then you can't robber the first one
-        vector<int> nums2(nums.begin() + 1, nums.end());
-        return max(robberLinear(nums1), robberLinear(nums2));
-    }
-private:
-    int robberLinear(vector<int>& nums) {
         if (nums.empty())
             return 0;
         if (nums.size() == 1)
             return nums[0];
-
-        vector<int> dp = {nums[0], nums[1]};
-
+        // if you robber the first one, then you can't robber the last one
+        vector<int> nums1(nums.begin(), nums.end() - 1);
+        // if you robber the last one, then you can't robber the first one
+        vector<int> nums2(nums.begin() + 1, nums.end());
+        return max(robberLinearOpt(nums1), robberLinearOpt(nums2));
+    }
+private:
+    int robberLinear(vector<int>& nums) {        
+        if (nums.size() == 1) {
+            return nums[0];
+        };
+        
+        vector<int> dp = {nums[0], max(nums[0], nums[1])}; // be careful!!!
+        
         for (int i = 2; i < nums.size(); i++)
-            dp.push_back(max(dp[i - 2] + nums[i], dp[i -1]));
+            dp.push_back(max(dp[i - 2] + nums[i], dp[i - 1]));
         
         return dp[dp.size() - 1];
+    }
+
+    int robberLinearII(vector<int>& nums) { // add 0 at first for dp, make code simpler
+        if (nums.size() == 1) {
+            return nums[0];
+        };
+        
+        vector<int> dp(nums.size() + 1, 0); // be careful!!!
+
+        dp[1] = nums[0];
+        for (int i = 2; i < nums.size(); i++)
+            dp.push_back(max(dp[i - 2] + nums[i - 1], dp[i - 1]));
+        
+        return dp[dp.size()];
+    }
+    int robberLinearOpt(vector<int>& nums) {  // space opt to O(1)
+        int odd = 0, even = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+             if (i % 2)
+                odd  = max(odd + nums[i], even);
+             else 
+                even = max(even + nums[i], odd);
+        }; 
+        return max(odd, even);
     }
 };
 
 int main(void) {
     Solution s;
-    vector<int> nums = {4, 5, 7, 9, 11, 12};
+    vector<int> nums = {1,3,1,3,100};
     cout << s.rob(nums) << endl;
     return 0;
 };
